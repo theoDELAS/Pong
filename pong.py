006c8099e583b0625af8480f249fd881ba592271
Théo <theo.delas@ynov.com>
2018-12-26 18:29:57 +0100

    Réorganisation du code : menu principal au lancement, bouton "play" pour ensuite lancer le jeu

--- pong.py
+++ pong.py
@@ -18,71 +18,67 @@ pos_y = (ecran_y // 2) - (fenetre_y //2)
 geo = f'{fenetre_x}x{fenetre_y}+{pos_x}+{pos_y}'
 fenetre.geometry(geo)
 
-# Création du canevas
+# Création du canvas
 canevas = Canvas(fenetre, width = fenetre_x, height = fenetre_y -60, bg = "black")
 
 # Coordonnées de la balle et vitesse
 x0 = fenetre_x // 2
 y0 = fenetre_y // 2
-dx = + 5
+dx = + 7
 dy = + 5
 
-#variable relatives aux raquettes
+# variables relatives aux raquettes
 debut_x1 = 0
 debut_x2 = 0
-#variable relatives à la balle
-y1 = 0
-y1 = 0
 
-# Création composants
-balle = canevas.create_oval(x0, y0, x0 + 20, y0 + 20, width = 2, fill = "red")
-joueur_1 = canevas.create_rectangle(debut_x1 + 5,50,debut_x1 + 20, 175, fill = "white")
-joueur_2 = canevas.create_rectangle(fenetre_x - 5, 150, fenetre_x -20, 275, fill = "white")
+def init_canvas_menu():
+    canevas.create_text(540, 100, fill = "white", font = ("Roboto", 75), text = "MENU PRINCIPAL")
+    bouton_jouer.pack()
+    
+# Création du canevas de jeu
+def init_canvas_jeu(): 
+    canevas.delete(ALL)
+    # Création composants
+    balle = canevas.create_oval(x0, y0, x0 + 20, y0 + 20, width = 2, fill = "red")
+    joueur_1 = canevas.create_rectangle(debut_x1 + 5,50,debut_x1 + 20, 175, fill = "white")
+    joueur_2 = canevas.create_rectangle(fenetre_x - 5, 150, fenetre_x -20, 275, fill = "white")
+    # déplacement de la raquettes
+    def bas1(event):
+        canevas.move(joueur_1,0,15)
+    def haut1(event):
+        canevas.move(joueur_1,0,-15)
+    def bas2(event):
+        canevas.move(joueur_2,0,15)
+    def haut2(event):
+        canevas.move(joueur_2,0,-15)
 
-#def déplacement de la raquettes
-def bas1(event):
-    canevas.move(joueur_1,0,15)
-def haut1(event):
-    canevas.move(joueur_1,0,-15)
-def bas2(event):
-    canevas.move(joueur_2,0,15)
-def haut2(event):
-    canevas.move(joueur_2,0,-15)
-
-canevas.bind_all('<Down>', bas2)
-canevas.bind_all('<Up>', haut2)
-canevas.bind_all('<s>', bas1)
-canevas.bind_all('<z>', haut1)
-# Déplacement perpetuel
-def deplacement():
-    global x0, y0, dx, dy
-
-    x0 = x0 + dx
-    y0 = y0 + dy
-
-    canevas.coords(balle, x0, y0, x0 + 20, y0 + 20)
-
-    if x0 < 0 or x0 > fenetre_x - 20:
-        dx = -dx
-    if y0 < 0 or y0 > fenetre_y - 80:
-        dy = -dy
-
-    canevas.after(20,deplacement)
-
-    return
-
-def action_deplacer():
+    canevas.bind_all('<Down>', bas2)
+    canevas.bind_all('<Up>', haut2)
+    canevas.bind_all('<s>', bas1)
+    canevas.bind_all('<z>', haut1)
+    # Déplacement perpetuel
+    def deplacement():
+        global x0, y0, dx, dy
+        x0 = x0 + dx
+        y0 = y0 + dy
+        canevas.coords(balle, x0, y0, x0 + 20, y0 + 20)
+        if x0 < 0 or x0 > fenetre_x - 20:
+            dx = -dx
+        if y0 < 0 or y0 > fenetre_y - 80:
+            dy = -dy
+        canevas.after(20,deplacement)
     deplacement()
-    return
+    bouton_menu.pack()
 
 # Création des bouttons
 boutton_quit = Button(fenetre, text = "Quit", command = fenetre.destroy)
-boutton_deplacement = Button(fenetre, text = "Play?", command = action_deplacer)
+bouton_menu = Button(fenetre, text = "Menu", command = init_canvas_menu)
+bouton_jouer = Button(fenetre, text = "Play", command = init_canvas_jeu)
 
-#placement des composants
-boutton_deplacement.pack()
+# placement des composants
 boutton_quit.pack()
 canevas.pack()
+init_canvas_menu()
 
-#boucle infinie
+# boucle infinie
 fenetre.mainloop()
